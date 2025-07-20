7ca7bce3764049c953bcfc90ae99733b84213805
Ignacio <laboroteca@gmail.com>
2025-07-20 21:08:45 +0200

    🧾 Mejora: Descripciones y nombres editoriales para productos

--- utils/productos.js
+++ utils/productos.js
@@ -2,15 +2,15 @@
 
 const PRODUCTOS = {
   'de cara a la jubilacion': {
-    nombre: 'De cara a la jubilación',
+    nombre: 'Libro digital con acceso vitalicio. De cara a la jubilación',
     slug: 'de-cara-a-la-jubilacion',
-    descripcion: 'Libro digital con acceso vitalicio',
+    descripcion: 'Libro digital con acceso vitalicio. De cara a la jubilación',
     price_id: 'price_1RMG0mEe6Cd77jenTpudZVan',
     imagen: 'https://www.laboroteca.es/wp-content/uploads/2025/06/DE-CARA-A-LA-JUBILACION-IGNACIO-SOLSONA-ABOGADO.png',
     precio_cents: 2990
   },
   'el club laboroteca': {
-    nombre: 'El Club Laboroteca',
+    nombre: 'Suscripción mensual a El Club Laboroteca. Acceso a contenido exclusivo.',
     slug: 'el-club-laboroteca',
     descripcion: 'Suscripción mensual a El Club Laboroteca. Acceso a contenido exclusivo.',
     price_id: 'price_1RmY1YEe6Cd77jenSc0mZxBi',

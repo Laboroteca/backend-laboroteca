7198b074ce9177eab35b782e203c6db8dde2af44
Ignacio <laboroteca@gmail.com>
2025-07-20 21:31:27 +0200

    ✅ Actualizado nombre y descripción de productos para que coincidan con campos del formulario

--- utils/productos.js
+++ utils/productos.js
@@ -1,7 +1,7 @@
 // utils/productos.js
 
 const PRODUCTOS = {
-  'de cara a la jubilacion': {
+  'libro digital con acceso vitalicio. de cara a la jubilacion': {
     nombre: 'Libro digital con acceso vitalicio. De cara a la jubilación',
     slug: 'de-cara-a-la-jubilacion',
     descripcion: 'Libro digital con acceso vitalicio. De cara a la jubilación',
@@ -9,17 +9,10 @@ const PRODUCTOS = {
     imagen: 'https://www.laboroteca.es/wp-content/uploads/2025/06/DE-CARA-A-LA-JUBILACION-IGNACIO-SOLSONA-ABOGADO.png',
     precio_cents: 2990
   },
-  'el club laboroteca': {
+  'alta y primera cuota mensual el club laboroteca. acceso a contenido exclusivo.': {
     nombre: 'Suscripción mensual a El Club Laboroteca. Acceso a contenido exclusivo.',
     slug: 'el-club-laboroteca',
     descripcion: 'Suscripción mensual a El Club Laboroteca. Acceso a contenido exclusivo.',
-    price_id: 'price_1RmY1YEe6Cd77jenSc0mZxBi',
-    imagen: 'https://www.laboroteca.es/wp-content/uploads/2025/06/Logo-Club-Laboroteca-ABOGADO.png'
-  },
-  'alta y cuota mensual en el club laboroteca. acceso a contenido exclusivo.': {
-    nombre: 'Alta y cuota mensual en El Club Laboroteca. Acceso a contenido exclusivo.',
-    slug: 'el-club-laboroteca',
-    descripcion: 'Alta y cuota mensual en El Club Laboroteca. Acceso a contenido exclusivo.',
     price_id: 'price_1RmY1YEe6Cd77jenSc0mZxBi',
     imagen: 'https://www.laboroteca.es/wp-content/uploads/2025/06/Logo-Club-Laboroteca-ABOGADO.png'
   }

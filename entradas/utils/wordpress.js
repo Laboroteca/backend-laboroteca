a989bb8f2ce8d4fe0a47e3e5712cf5156f253c04
Ignacio <laboroteca@gmail.com>
2025-07-30 13:16:39 +0200

    ✨ Crear sesión Stripe para entradas con asistentes y metadata personalizada

---
+++ entradas/utils/wordpress.js
@@ -0,0 +1,6 @@
+async function emailRegistradoEnWordPress(email) {
+  console.log('🔓 Verificación desactivada. Email:', email);
+  return true;
+}
+
+module.exports = { emailRegistradoEnWordPress };

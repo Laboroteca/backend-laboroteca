1f43faf2fee2ae5a778d6e19f21c8a6404d4d52e
Ignacio <laboroteca@gmail.com>
2025-08-19 11:55:20 +0200

    Mi Cuenta: listado de entradas desde Firestore y reenvío desde GCS

--- index.js
+++ index.js
@@ -98,6 +98,8 @@ app.use('/regalos', require('./regalos/routes/crear-codigo-regalo'));
 app.use('/entradas/crear', require('./entradas/routes/crearEntrada'));
 app.use('/entradas/sesion', require('./entradas/routes/create-session-entrada'));
 
+app.use('/', require('./routes/micuentaEntradas'));
+
 app.use('/', validarEntrada);
 
 const pagoLimiter = rateLimit({

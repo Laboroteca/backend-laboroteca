894cf7b1c4af6ed7faaec32b9c59dd021f90d24e
Ignacio <laboroteca@gmail.com>
2025-08-20 17:21:09 +0200

    Prueba: provocar fallo Firestore (cambio temporal)

--- firebase.js
+++ firebase.js
@@ -19,9 +19,9 @@ try {
 }
 
 if (!admin.apps.length) {
-// ❌ provoca fallo
-// admin.initializeApp({ credential: admin.credential.cert(svc) });
-
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+  });
   if (process.env.NODE_ENV !== 'production') {
     console.log('✅ Firebase inicializado');
   }
